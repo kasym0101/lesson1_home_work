343b54b5917a2aef1e9b9e201a79a1feec5b25e6
Kasym0101 <kasym.dzhorobaev@gmail.com>
2023-05-15 16:19:55 +0600

    first commit

---
+++ bin/lesson1_home_work.dart
@@ -0,0 +1,4 @@
+void main() {
+  print('My name is Kasym.');
+  print('I am 28 years old.');
+}
---
+++ test/lesson1_home_work_test.dart
@@ -0,0 +1,8 @@
+import 'package:lesson1_home_work/lesson1_home_work.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
